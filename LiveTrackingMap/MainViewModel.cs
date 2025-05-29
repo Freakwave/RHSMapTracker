0e2b924a0d58f6f6ed17bdb08d0e7aa42b38cde8
Freakwave <mares.eric91@gmail.com>
2025-05-29 22:22:38 +0200

    Refactor + SVG for Dog position + Better Coloring

--- LiveTrackingMap/MainViewModel.cs
+++ LiveTrackingMap/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using LiveTrackingMap.Models;
+
 // Assuming your service and models are directly in LiveTrackingMap
 // If they are in sub-namespaces like LiveTrackingMap.Services, add:
 // using LiveTrackingMap.Services; 
@@ -38,139 +40,6 @@ using Pen = Mapsui.Styles.Pen; // For Application.Current.Dispatcher
 // if they are in sub-namespaces of LiveTrackingMap.
 namespace LiveTrackingMap // Or just LiveTrackingMap if MainViewModel is there
 {
-    // Helper class DogTrackMapObject (can be in this file or its own .cs file within the namespace)
-    public partial class DogTrackMapObject : ObservableObject
-    {
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(DisplayName))]
-        private string _deviceName = string.Empty;
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(DisplayName))]
-        private string _customName = string.Empty;
-
-
-        public string DisplayName
-        {
-            get
-            {
-                if(CustomName == DeviceName || CustomName == String.Empty)
-                {
-                    return DeviceName;
-                }
-                else
-                {
-                    return $"{CustomName} ({DeviceName})";
-                }
-            }
-        }
-
-        [ObservableProperty]
-        private MPoint _currentMapPositionMPoint;
-        // This list will store all NTS coordinates for the track
-        private List<Coordinate> _trackPathNtsCoordinates = new List<Coordinate>();
-
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(DogColorBrush))]
-        private Color _dogColor;
-
-        partial void OnDogColorChanged(Color value)
-        {
-            PathMapFeature?.Styles.OfType<VectorStyle>().FirstOrDefault()?.Line = DogColorPen;
-            LastPositionMarkerMapFeature?.Styles.OfType<SymbolStyle>().FirstOrDefault()?.Fill = DogColorBrush;
-        }
-        public Brush DogColorBrush => new Brush(DogColor);
-        public Pen DogColorPen => new Pen(DogColor, 3);
-
-
-
-        public LineString TrackGeometryNts { get; private set; }
-        public GeometryFeature PathMapFeature { get; }
-
-        [ObservableProperty]
-        private PointFeature lastPositionMarkerMapFeature;
-
-        public MPoint CurrentPosition
-        {
-            get
-            {
-                return LastPositionMarkerMapFeature.Point;
-            }
-        }
-
-        public List<MPoint>? Track => PathMapFeature?.Geometry?.Coordinates
-                                    .Select(coord => coord.ToMPoint())
-                                    .Reverse()
-                                    .ToList();
-
-
-        public double TrackLength => PathMapFeature?.Geometry?.Length ?? 0.0;
-
-        public DogTrackMapObject(string name, Color trackColor)
-        {
-            DeviceName = name;
-            DogColor = trackColor;
-
-            PathMapFeature = new GeometryFeature(new LineString(Array.Empty<Coordinate>()));
-            PathMapFeature.Styles.Add(new VectorStyle { Line = DogColorPen });
-            PathMapFeature["Name"] = $"{DisplayName} Path";
-
-            LastPositionMarkerMapFeature = new PointFeature(0, 0);
-            LastPositionMarkerMapFeature["Name"] = DisplayName;
-            LastPositionMarkerMapFeature.Styles.Add(new SymbolStyle
-            {
-                SymbolType = SymbolType.Ellipse,
-                SymbolScale = 0.7,
-                Fill = DogColorBrush,
-                Outline = new Pen(Color.Black, 1)
-            });
-            LastPositionMarkerMapFeature.Styles.Add(new LabelStyle
-            {
-                LabelColumn = "Name",
-                Font = new Font { FontFamily = "Arial", Size = 10 },
-                Offset = new Offset(0, -15),
-                BackColor = new Brush(Color.FromArgb(180, 255, 255, 255)),
-                Halo = new Pen(Color.FromArgb(120, 255, 255, 255), 1)
-            });
-            _currentMapPositionMPoint = new MPoint(0, 0);
-        }
-
-        public void UpdateName(string newName)
-        {
-            DeviceName = newName;
-            LastPositionMarkerMapFeature["Name"] = DisplayName;
-            PathMapFeature["Name"] = $"{DisplayName} Path"; 
-        }
-
-        public void AddPoint(double longitudeDegrees, double latitudeDegrees)
-        {
-            MPoint newMapPoint = SphericalMercator.FromLonLat(longitudeDegrees, latitudeDegrees).ToMPoint();
-            var ntsCoordinate = newMapPoint.ToCoordinate();
-
-            // Add the new coordinate to the list of vertices for the NTS LineString
-            _trackPathNtsCoordinates.Add(ntsCoordinate);
-
-            if (_trackPathNtsCoordinates.Count >= 2)
-            {
-                PathMapFeature.Geometry = new LineString(_trackPathNtsCoordinates.ToArray());
-            }
-            else
-            {
-                PathMapFeature.Geometry = new LineString(Array.Empty<Coordinate>());
-            }
-
-            LastPositionMarkerMapFeature.Point.X = newMapPoint.X;
-            LastPositionMarkerMapFeature.Point.Y = newMapPoint.Y;
-
-
-
-            OnPropertyChanged(nameof(CurrentPosition));
-            OnPropertyChanged(nameof(TrackLength));
-            OnPropertyChanged(nameof(Track));
-
-            CurrentMapPositionMPoint = newMapPoint;
-        }
-    }
-
     public partial class MainViewModel : ObservableObject
     {
         private readonly GarminUsbService _garminService; // Assuming GarminUsbService is in LiveTrackingMap or LiveTrackingMap.Services
--- LiveTrackingMap/Models/DogTrackMapObject.cs
+++ LiveTrackingMap/Models/DogTrackMapObject.cs
@@ -3,6 +3,7 @@ using Mapsui;
 using Mapsui.Nts;
 using Mapsui.Styles;
 using Mapsui.Projections;
+using Mapsui.Rendering;
 using NetTopologySuite.Geometries;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ using System.ComponentModel;
 using Mapsui.Layers;
 using Mapsui.Nts.Extensions;
 using Mapsui.Extensions;
+using System.IO;
 
 namespace LiveTrackingMap.Models
 {
@@ -46,16 +48,16 @@ namespace LiveTrackingMap.Models
 
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(DogColorBrush))]
+        [NotifyPropertyChangedFor(nameof(DogColorPen))]
+        [NotifyPropertyChangedFor(nameof(ContrastDogColor))]
+        [NotifyPropertyChangedFor(nameof(ContrastDogColorBrush))]
         private Color _dogColor;
-
-        partial void OnDogColorChanged(Color value)
-        {
-            PathMapFeature?.Styles.OfType<VectorStyle>().FirstOrDefault()?.Line = DogColorPen;
-            LastPositionMarkerMapFeature?.Styles.OfType<SymbolStyle>().FirstOrDefault()?.Fill = DogColorBrush;
-        }
 
         public Brush DogColorBrush => new Brush(DogColor);
         public Pen DogColorPen => new Pen(DogColor, 3);
+
+        public Color ContrastDogColor => GetContrastingForegroundColor(DogColor);
+        public Brush ContrastDogColorBrush => new Brush(ContrastDogColor);
 
         public LineString TrackGeometryNts { get; private set; }
         public GeometryFeature PathMapFeature { get; }
@@ -72,6 +74,42 @@ namespace LiveTrackingMap.Models
 
         public double TrackLength => PathMapFeature?.Geometry?.Length ?? 0.0;
 
+
+
+
+        private static int? _dogBitmapId;
+        private static int GetDogBitmapId()
+        {
+            if (_dogBitmapId.HasValue)
+            {
+                return _dogBitmapId.Value;
+            }
+
+            using var resourceStream = System.Reflection.Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("LiveTrackingMap.Assets.paw.svg");
+
+            if (resourceStream == null)
+            {
+                _dogBitmapId = -1;
+                return -1;
+            }
+
+            var permanentStream = new MemoryStream();
+            resourceStream.CopyTo(permanentStream);
+            permanentStream.Position = 0;
+
+            _dogBitmapId = BitmapRegistry.Instance.Register(permanentStream);
+            return _dogBitmapId.Value;
+        }
+
+        partial void OnDogColorChanged(Color value)
+        {
+            PathMapFeature?.Styles.OfType<VectorStyle>().FirstOrDefault()?.Line = DogColorPen;
+            LastPositionMarkerMapFeature?.Styles.OfType<SymbolStyle>().FirstOrDefault()?.Line = DogColorPen;
+            LastPositionMarkerMapFeature?.Styles.OfType<LabelStyle>().FirstOrDefault()?.BackColor = DogColorBrush;
+            LastPositionMarkerMapFeature?.Styles.OfType<LabelStyle>().FirstOrDefault()?.ForeColor = GetContrastingForegroundColor(DogColor); ;
+        }
+
         public DogTrackMapObject(string name, Color trackColor)
         {
             DeviceName = name;
@@ -83,22 +121,36 @@ namespace LiveTrackingMap.Models
 
             LastPositionMarkerMapFeature = new PointFeature(0, 0);
             LastPositionMarkerMapFeature["Name"] = DisplayName;
+
+            // Create bitmap style for the dog marker
             LastPositionMarkerMapFeature.Styles.Add(new SymbolStyle
             {
-                SymbolType = SymbolType.Ellipse,
-                SymbolScale = 0.7,
-                Fill = DogColorBrush,
-                Outline = new Pen(Color.Black, 1)
+                BitmapId = GetDogBitmapId(),
+                SymbolType = SymbolType.Image,
+                UnitType = UnitType.Pixel,
+                SymbolScale = 0.04,  
+                Opacity = 0.8f,        
+                SymbolOffset = new Offset(0, 0),
+                Line = DogColorPen,
             });
+
+            // Add label style
             LastPositionMarkerMapFeature.Styles.Add(new LabelStyle
             {
                 LabelColumn = "Name",
                 Font = new Font { FontFamily = "Arial", Size = 10 },
-                Offset = new Offset(0, -15),
-                BackColor = new Brush(Color.FromArgb(180, 255, 255, 255)),
-                Halo = new Pen(Color.FromArgb(120, 255, 255, 255), 1)
+                Offset = new Offset(0, -25), // Adjusted offset to account for image height
+                BackColor = DogColorBrush,
+                ForeColor = GetContrastingForegroundColor(DogColor)
+                //Halo = new Pen(Color.FromArgb(120, 255, 255, 255), 1)
             });
             _currentMapPositionMPoint = new MPoint(0, 0);
+        }
+
+        public static Color GetContrastingForegroundColor(Color backgroundColor)
+        {
+            double luminance = (0.299 * backgroundColor.R + 0.587 * backgroundColor.G + 0.114 * backgroundColor.B) / 255;
+            return luminance > 0.5 ? Color.Black : Color.White;
         }
 
         public void UpdateName(string newName)
